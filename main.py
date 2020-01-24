32255c2cee3e7d121bdd1ce95c42fc1fa40b6b15
Alif22 <alhaikal22@gmail.com>
2020-01-24 16:28:25 +0800

    use input for r instead of keypressed

--- main.py
+++ main.py
@@ -17,29 +17,32 @@ if __name__ == "__main__":
     holdTIme = 1
     programRun = True
     while programRun:
-        print("Press R to run the program")
-        try:
-            if keyboard.is_pressed('r'):
-                programRun = True
-        except:
-            print("Press the fucking R you blindman. Im done. Rage quitting")
-        print("running the program in 5 seconds. Click your game")
-        time.sleep(1)
-        print("1")
-        time.sleep(1)
-        print("2")
-        time.sleep(1)
-        print("3")
-        time.sleep(1)
-        print("4")
-        time.sleep(1)
-        AMRun = True
+        Input = input("Enter r to run program:")
+        AMRun = False
+        if Input.lower() == 'r':
+                AMRun = True
+        else:
+            print("Thats not R u retard!!")
+            print("Im done. Rage quitting")
+            time.sleep(2)
+        
         while AMRun and programRun:
+            print("running the program in 5 seconds. Click your game")
+            time.sleep(1)
+            print("1")
+            time.sleep(1)
+            print("2")
+            time.sleep(1)
+            print("3")
+            time.sleep(1)
+            print("4")
+            time.sleep(1)
             #press b to stop move
             try:
                 if keyboard.is_pressed('b'):
                     AMRun = False
             except:
+                print("do nothing")
                 #do nothing just catchin error
             mapStillOnScreen = True
             if mapStillOnScreen:
@@ -47,5 +50,5 @@ if __name__ == "__main__":
                 mapStillOnScreen = keyPressForTime(holdTime,'s')
                 mapStillOnScreen = keyPressForTime(holdTime,'a')
                 mapStillOnScreen = keyPressForTime(holdTime,'w')
-            else
+            else:
                 AMRun = False
